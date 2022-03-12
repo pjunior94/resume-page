96da17fd6e1fa61d5f09181fbf0eadacf0a53162
Paulo Henrique Junior <ps.junior94@gmail.com>
2022-03-12 12:15:35 +0000

    Updates

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import PageIndex from "./pages/index/page-index";
-
-function App() {
-	return <PageIndex />;
-}
-
-export default App;
--- src/models/Company.ts
+++
@@ -1,9 +0,0 @@
-export class Company {
-	name: string;
-	description: string;
-
-	constructor(company: Company) {
-		this.name = company.name;
-		this.description = company.description;
-	}
-}
